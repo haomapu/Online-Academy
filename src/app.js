061974f1d2449e4a5c35c89d07d92d7b999f5efb
Hoang Mai <maihoang2152002@gmail.com>
2022-12-08 23:22:26 +0700

    search page: filter, one course(demo)

--- src/app.js
+++ src/app.js
@@ -1,47 +1,58 @@
-import express from 'express';
-import { engine } from 'express-handlebars';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import path from 'path'
-
+import express from "express";
+import { engine } from "express-handlebars";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
 //Import Router
-import homepageRouter from './routes/homepage.route.js'
-import detailsRouter from './routes/details.route.js'
-
+import homepageRouter from "./routes/homepage.route.js";
+import detailsRouter from "./routes/details.route.js";
+import searchPageRouter from "./routes/searchPage.route.js";
 
 //Const variable
 const app = express();
-app.use('/public', express.static('public'));
+app.use("/public", express.static("public"));
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const PORT = 5000;
-
+const PORT = 8080;
 
 //Set up bootstrap
-app.use('/css',express.static(path.join(__dirname ,'..', 'node_modules/bootstrap/dist/css')))
-app.use('/js',express.static(path.join(__dirname ,'..', 'node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname ,'..', 'node_modules/jquery/dist')))
+app.use(
+    "/css",
+    express.static(
+        path.join(__dirname, "..", "node_modules/bootstrap/dist/css")
+    )
+);
+app.use(
+    "/js",
+    express.static(path.join(__dirname, "..", "node_modules/bootstrap/dist/js"))
+);
+app.use(
+    "/js",
+    express.static(path.join(__dirname, "..", "node_modules/jquery/dist"))
+);
 
 //Set up view engine
-app.engine('hbs', engine({
-  defaultLayout: 'main.handlebars'
-  
-}));
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.engine(
+    "hbs",
+    engine({
+        defaultLayout: "main.handlebars",
+    })
+);
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
 
 //Default homepage
-app.get('/', (req, res) => {
-    res.render('home');
+app.get("/", (req, res) => {
+    res.render("home");
 });
 
 //Router
-app.use('/test', homepageRouter)
-app.use('/details', detailsRouter)
-
+app.use("/test", homepageRouter);
+app.use("/details", detailsRouter);
+app.use("/searchPage", searchPageRouter);
 
 //Start App
 app.listen(PORT, function () {
-  console.log(`Online Academy listening at http://localhost:${PORT}`);
+    console.log(`Online Academy listening at http://localhost:${PORT}`);
 });
