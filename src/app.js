7b0af9481954691b5e68afa3d779820e9a975426
Hoang Mai <maihoang2152002@gmail.com>
2022-12-09 23:04:15 +0700

    fix some duplicate import

--- src/app.js
+++ src/app.js
@@ -1,26 +1,16 @@
+//Import Router
+import searchPageRouter from "./routes/searchPage.route.js";
 import express from "express";
 import { engine } from "express-handlebars";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import path from "path";
+import hbs_sections from "express-handlebars-sections";
 
 //Import Router
+import mongoose from "mongoose";
 import homepageRouter from "./routes/homepage.route.js";
 import detailsRouter from "./routes/details.route.js";
-import searchPageRouter from "./routes/searchPage.route.js";
-import express from 'express';
-import { engine } from 'express-handlebars';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import path from 'path'
-import hbs_sections from 'express-handlebars-sections';
-
-
-//Import Router
-import mongoose from 'mongoose';
-import homepageRouter from './routes/homepage.route.js'
-import detailsRouter from './routes/details.route.js'
-
 
 //Const variable
 const app = express();
@@ -30,16 +20,27 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const PORT = 8080;
 
 //Set up bootstrap
-app.use('/css',express.static(path.join(__dirname ,'..', 'node_modules/bootstrap/dist/css')))
-app.use('/js',express.static(path.join(__dirname ,'..', 'node_modules/bootstrap/dist/js')))
+app.use(
+    "/css",
+    express.static(
+        path.join(__dirname, "..", "node_modules/bootstrap/dist/css")
+    )
+);
+app.use(
+    "/js",
+    express.static(path.join(__dirname, "..", "node_modules/bootstrap/dist/js"))
+);
 
 //Set up view engine
-app.engine('hbs', engine({
-  defaultLayout: 'main.handlebars',
-  section: hbs_sections()  
-}));
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.engine(
+    "hbs",
+    engine({
+        defaultLayout: "main.handlebars",
+        section: hbs_sections(),
+    })
+);
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
 
 //Default homepage
 app.get("/", (req, res) => {
