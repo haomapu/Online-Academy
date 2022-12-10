d518584dcbc141912c9462d31af8775d56be4a8e
baonguyen25 <nguyenminhgiabao2501@gmail.com>
2022-12-10 17:10:43 +0700

    implement service for main pages

---
+++ src/controllers/main.service.js
@@ -0,0 +1,20 @@
+const mainService = {
+    getHomePage : async(req, res) => {
+        res.render("home");
+    },
+
+    getSearchPage: async(req, res) => {
+        res.render("vwSearchPage/searchPage");
+    },
+    
+    getCourseDetail: async(req, res) => {
+        const id = req.params.id;
+        const list = await detailService.findAll();
+        res.render('vwDetails/details', {
+            detail: list[id],
+            empty: list.length === 0
+        });
+    },
+};
+
+export default mainService;
