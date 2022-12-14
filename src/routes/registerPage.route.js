9f64b8d18abc833bea44bfb91ad297455abb0f0f
shinkyung <ngogiahai014@gmail.com>
2022-12-14 15:04:00 +0700

    fix register

--- src/routes/registerPage.route.js
+++ src/routes/registerPage.route.js
@@ -7,19 +7,19 @@ router.get("/", function (req, res) {
     res.render("vwRegisterPage/registerPage");
 });
 
-router.post('/register', function(req, res, next) {
-    var salt = crypto.randomBytes(16);
-    crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
-      if (err) { return next(err); }
-      const user = {
-        fullname: req.body.name,
-        password: hashedPassword,
-        email: req.body.email,
-        permission: 0
-      }
-      User.createUser(user);
-      req.render("/");
-    });
-  });
+// router.post('/register', function(req, res, next) {
+//     var salt = crypto.randomBytes(16);
+//     crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+//       if (err) { return next(err); }
+//       const user = {
+//         fullname: req.body.name,
+//         password: hashedPassword,
+//         email: req.body.email,
+//         permission: 0
+//       }
+//       User.createUser(user);
+//       req.render("/");
+//     });
+//   });
 
 export default router;
