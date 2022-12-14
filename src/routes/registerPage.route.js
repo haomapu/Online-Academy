e105aefcc77e1f6fd9fbef9195f0206e84533101
shinkyung <ngogiahai014@gmail.com>
2022-12-14 14:06:15 +0700

    fix form

--- src/routes/registerPage.route.js
+++ src/routes/registerPage.route.js
@@ -1,4 +1,5 @@
 import express from "express";
+import User from "../controllers/users.service";
 
 const router = express.Router();
 
@@ -10,17 +11,14 @@ router.post('/register', function(req, res, next) {
     var salt = crypto.randomBytes(16);
     crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
       if (err) { return next(err); }
-        db.run('INSERT INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [req.body.username,hashedPassword,salt], function(err) {
-        if (err) { return next(err); }
-        var user = {
-          id: this.lastID,
-          username: req.body.username
-        };
-        req.login(user, function(err) {
-          if (err) { return next(err); }
-          res.redirect('/');
-        });
-      });
+      const user = {
+        fullname: req.body.name,
+        password: hashedPassword,
+        email: req.body.email,
+        permission: 0
+      }
+      User.createUser(user);
+      req.render("/");
     });
   });
 
