19fab538f909b2df6233c8b654505e6bb23c6134
haomapu <haoanh7702@gmail.com>
2022-12-15 17:43:40 +0700

    Fix conflicts

--- src/app.js
+++ src/app.js
@@ -8,12 +8,9 @@ import hbs_sections from "express-handlebars-sections";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-<<<<<<< HEAD
 import numeral from 'numeral';
 
-=======
 import passport from "passport";
->>>>>>> b1573f558d702f6d4b35214a9e532c0d0a3db264
 //Inport router
 
 import homepageRouter from "./routes/homepage.route.js";
