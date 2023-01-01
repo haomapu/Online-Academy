87ecd090cb6e5037c8266b95454757ae55e7dfeb
haiphung1501 <haiphung15012002@gmail.com>
2023-01-01 23:08:35 +0700

    [navBar, Model] fix Navbar, fix category model

--- src/models/category.js
+++ src/models/category.js
@@ -4,7 +4,13 @@ const categorySchema = new mongoose.Schema({
     name: {
         type: String,
         require: true,
-    }
+    },
+    sub_categories: [
+        {
+            type: mongoose.Schema.Types.Object,
+            ref: "SubCategory",
+        },
+    ],
 });
 
 const Category = mongoose.model("Category", categorySchema);
