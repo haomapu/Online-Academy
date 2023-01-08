55f2b080c3acab703199ab7b444bc9a97fcbbffb
shinkyung <ngogiahai014@gmail.com>
2023-01-08 21:44:20 +0700

    add model, test post and get video

--- src/routes/main.route.js
+++ src/routes/main.route.js
@@ -1,8 +1,22 @@
 import express from "express";
 import mainService from "../controllers/main.service.js";
+import multer from "multer";
+
 const router = express.Router();
 
+var storage = multer.diskStorage({
+    filename: function (req, file, cb) {
+    cb(null, file.originalname);
+    }
+  });
+var upload = multer({ storage: storage});
+
+
 router.get("/", mainService.getHomePage);
+
+router.get("/test", mainService.test);
+
+router.post("/test", upload.single('picture'), mainService.testUpload);
 
 router.get("/search", mainService.getSearchCourses);
 
