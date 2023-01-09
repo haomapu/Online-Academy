86ad84303b944df3fa5f7f869615be35ebfa1d2f
haiphung1501 <haiphung15012002@gmail.com>
2023-01-09 17:00:32 +0700

    [Authorization] Finish user authorization

---
+++ src/middlewares/auth.mdw.js
@@ -0,0 +1,11 @@
+export default function authWithRequiredPermission(requiredPermission) {
+  return function (req, res, next) {
+    if (req.isAuthenticated()) {
+      if (req.user && req.user.role < requiredPermission) {
+        return res.render('/403', { layout: false });
+      }
+      next();
+    }
+
+  }
+}
